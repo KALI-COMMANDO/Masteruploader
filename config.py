75e6347adf7ef5ceb25160d3c3ca1db7eab91b0b
KALI-COMMANDO <Kaalicommandobot@gmail.com>
2025-05-25 04:05:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    AUTH_USER = os.environ.get('AUTH_USERS', '7602994049').split(',')
+    BOT_TOKEN = os.environ.get("7706666288:AAGRPPgROZBjWTLfXuPfgCTZG5hsRBK-NTw")
+    API_ID = int(os.environ.get("28116598"))
+    API_HASH = os.environ.get("8e148ca61f021189f6f72f8d5723d9ab")
+    AUTH_USER = os.environ.get('AUTH_USERS', '6631510369').split(',')
     AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
     HOST = "https://drm-api-six.vercel.app"
     CREDIT = " 𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎"
